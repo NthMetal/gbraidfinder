1ed82972bdb7fe3d8f05db2809e3e48504a96f2e
nthmetal <832529@gmail.com>
2022-11-08 22:54:03 -0500

    cleanup frontend code, add comments

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, ElementRef, OnDestroy, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { MatChipInputEvent } from '@angular/material/chips';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
@@ -7,10 +6,8 @@ import { DragulaService } from 'ng2-dragula';
 import { MetadataService } from './shared/services/metadata.service';
 import { SocketioService } from './shared/services/socketio.service';
 
-import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import { FormControl, NgForm } from '@angular/forms';
-import { debounceTime, distinctUntilChanged, map, Observable, startWith, Subject, Subscription } from 'rxjs';
-import { MatAutocompleteSelectedEvent, MatAutocompleteTrigger } from '@angular/material/autocomplete';
+import { NgForm } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -18,11 +15,8 @@ import { MatAutocompleteSelectedEvent, MatAutocompleteTrigger } from '@angular/m
   styleUrls: ['./app.component.less']
 })
 export class AppComponent implements OnInit, OnDestroy {
-  @ViewChild('searchInput') searchInput: ElementRef;
-  @ViewChild(MatAutocompleteTrigger, { read: MatAutocompleteTrigger }) matAutoComplete: MatAutocompleteTrigger;
   @ViewChild('settingsForm') settingsForm: NgForm;
 
-  separatorKeysCodes: number[] = [ENTER, COMMA];
   raid_metadata: any[] = [];
   subscribedRaids: any[] = [];
 
@@ -49,14 +43,19 @@ export class AppComponent implements OnInit, OnDestroy {
 
   formChangesSubscription: Subscription;
 
-  subbedQuestsCtrl = new FormControl();
-  filteredQuests: Observable<any[]>;
-
   usersConnected = 0;
 
   raidSearch = '';
   raidSearchSubject = new Subject<string>();
   raidSearchFiltered: { standard: { [key: string]: any[] }, impossible: { [key: string]: any[] } };
+  
+  elements: string[] = ['fire', 'water', 'earth', 'wind', 'light', 'dark', 'normal']
+  joinLevels: number[] = []
+  groupedRaids: { standard: { [key: string]: any[] }, impossible: { [key: string]: any[] } };
+  standardDifficulties: string[] = [];
+  impossibleDifficulties: string[] = [];
+
+  tabActive = true;
 
   constructor(
     private router: Router,
@@ -67,15 +66,15 @@ export class AppComponent implements OnInit, OnDestroy {
     private dragulaService: DragulaService
   ) { }
 
-  elements: string[] = ['fire', 'water', 'earth', 'wind', 'light', 'dark', 'normal']
-  joinLevels: number[] = []
-  groupedRaids: { standard: { [key: string]: any[] }, impossible: { [key: string]: any[] } };
-  standardDifficulties: string[] = [];
-  impossibleDifficulties: string[] = [];
-
-  tabActive = true;
-
+  /**
+   * ng init, runs once on page load
+   * see method for specific initializations
+   */
   async ngOnInit(): Promise<void> {
+    /**
+     * Adds listener for when the user tabs out
+     * Used to limit raids that appear when tabbed out
+     */
     document.addEventListener("visibilitychange", (event) => {
       if (document.visibilityState == "visible") {
         this.tabActive = true;
@@ -86,30 +85,34 @@ export class AppComponent implements OnInit, OnDestroy {
       }
     });
 
+    /**
+     * Get settings from local storage.
+     */
     this.settings = JSON.parse(localStorage.getItem('settings') || JSON.stringify(this.settings));
-
-    this.filteredQuests = this.subbedQuestsCtrl.valueChanges.pipe(
-      startWith(null),
-      map(raid => raid ? this._filter(raid) : this.raid_metadata.slice()));
     
-    this.raidSearchSubject.pipe(
-      debounceTime(400),
-      distinctUntilChanged())
-      .subscribe(value => {
-        if (value === '') return this.raidSearchFiltered = this.groupedRaids;
-        const filterValue = value.toLowerCase();
-        return this.raidSearchFiltered = {
-          standard: this.standardDifficulties.reduce((acc, difficulty) => {
-            acc[difficulty] = this.groupedRaids.standard[difficulty].filter(raid => Object.values(raid).join().toLowerCase().includes(filterValue));
-            return acc;
-          }, {} as any),
-          impossible: this.impossibleDifficulties.reduce((acc, difficulty) => {
-            acc[difficulty] = this.groupedRaids.impossible[difficulty].filter(raid => Object.values(raid).join().toLowerCase().includes(filterValue));
-            return acc;
-          }, {} as any)
-        }
-      });
+    /**
+     * Subscribes to raid search subject
+     * updates raidSearchFiltered variable with the new value
+     */
+    this.raidSearchSubject.pipe(debounceTime(400), distinctUntilChanged()).subscribe(value => {
+      if (value === '') return this.raidSearchFiltered = this.groupedRaids;
+      const filterValue = value.toLowerCase();
+      return this.raidSearchFiltered = {
+        standard: this.standardDifficulties.reduce((acc, difficulty) => {
+          acc[difficulty] = this.groupedRaids.standard[difficulty].filter(raid => Object.values(raid).join().toLowerCase().includes(filterValue));
+          return acc;
+        }, {} as any),
+        impossible: this.impossibleDifficulties.reduce((acc, difficulty) => {
+          acc[difficulty] = this.groupedRaids.impossible[difficulty].filter(raid => Object.values(raid).join().toLowerCase().includes(filterValue));
+          return acc;
+        }, {} as any)
+      }
+    });
 
+    /**
+     * Initializes the dragula group
+     * configures the columns to be dragable
+     */
     this.dragulaService.createGroup("DRAGABLE_COLUMNS", {
       direction: 'horizontal',
       moves: (el, container, handle) => {
@@ -117,6 +120,10 @@ export class AppComponent implements OnInit, OnDestroy {
       }
     });
 
+    /**
+     * Load raid metadata. A list of all the raids and info about them.
+     * from the backend
+     */
     this.raid_metadata = await new Promise(resolve => {
       this.metadataService.getRaidMetadata().subscribe(metadata => {
         resolve(metadata);
@@ -124,8 +131,14 @@ export class AppComponent implements OnInit, OnDestroy {
     });
     console.log(this.raid_metadata)
 
-    // this.joinLevels = [...new Set(this.raid_metadata.map(raid => raid.level))].sort((a, b) => a - b)
-    // this.toggleRaid('305171');
+    /**
+     * Creates specific groups from the list of all the raids that we just got above
+     * Also groups these raids by their difficulty (stars in gbf ui)
+     * Creates a group for filtered raids with no filter (all raids)
+     * Creates a group for standard difficulty raids.
+     * Creates a group for impossible difficulty raids.
+     * TODO Future: Create a group for event/other raids
+     */
     this.groupedRaids = this.raid_metadata.reduce((acc, curr) => {
       if (acc[curr.impossible === 1 ? 'standard' : 'impossible'][curr.thumbnail_image]) {
         acc[curr.impossible === 1 ? 'standard' : 'impossible'][curr.thumbnail_image].push(curr);
@@ -136,8 +149,13 @@ export class AppComponent implements OnInit, OnDestroy {
     this.raidSearchFiltered = this.groupedRaids;
     this.standardDifficulties = Object.keys(this.groupedRaids.standard);
     this.impossibleDifficulties = Object.keys(this.groupedRaids.impossible);
-    this.impossibleDifficulties = Object.keys(this.groupedRaids.impossible);
 
+    /**
+     * Handles a subscription event for when a user subscribes or unsubscribes to a raid
+     * Creates a list of subscribed raids
+     * Updates the 'selected' value for the filtered raids (so that it could show the border)
+     * Updates the 'selected' value for grouped raids so that value could be used elsewhere 
+     */
     this.socketioService.subscribedRaids.subscribe(subbed => {
       const subbedRaids: any[] = [];
       subbed.forEach(subbedRaid => {
@@ -155,11 +173,25 @@ export class AppComponent implements OnInit, OnDestroy {
       });
       // this.subscribedRaids = this.raid_metadata.filter(raidm => subbed.has('r' + raidm.quest_id));
     });
+
+    /**
+     * Subscribes to status event from socket
+     * curretnly doesn't really do anything with it except log it
+     * usersConnected is just for monitoring, and might be inaccurate
+     */
     this.socketioService.fromEvent('status').subscribe((status: string) => {
       const parsed = JSON.parse(status);
       this.usersConnected = parsed.usersConnected;
       console.log('GOT STATUS', status);
     });
+    
+    /**
+     * Subscribes to raid event from socket
+     * This is a basic raid info. Battle key and twitter user info.
+     * Adds it to the raid list for the relevant quest Id so it shows up
+     * Creates a set timeout to remove the raid after some amount of time
+     * Also pops a raid from the list when tabbed out as to not increase list size
+     */
     this.socketioService.getRaids().subscribe(raid => {
       if (this.raids[raid.quest_id]) {
         this.raids[raid.quest_id].unshift(raid);
@@ -171,6 +203,14 @@ export class AppComponent implements OnInit, OnDestroy {
         if (index >= 0) this.raids[raid.quest_id].splice(index, 1);
       }, 1000 * this.settings.removeRaidsAfterSeconds);
     });
+
+    /**
+     * Subscribes to update events from socket
+     * These include info such as hp, players, time left, host class
+     * Finds the raid and adds the updated info to it
+     * does nothing if raid or quest DNE
+     * Removes the raid if it hp/player filters are failed
+     */
     this.socketioService.getUpdates().subscribe(update => {
       const raidIndex = this.raids[update.questID]?.findIndex(raid => raid.battleKey === update.battleKey);
       if (!this.raids[update.questID] || raidIndex === -1) return;
@@ -181,10 +221,15 @@ export class AppComponent implements OnInit, OnDestroy {
       const minPlayers = this.settings.minPlayers === 0 ? -1 : this.settings.minPlayers;
 
       if (update.hp <= this.settings.minHP || update.hp > this.settings.maxHP || players <= minPlayers || players > maxPlayers) {
+        console.log(`removed raid ${raid.battleKey} > ${this.settings.minHP}<${update.hp}<=${this.settings.maxHP}?   ${minPlayers}<${players}<=${maxPlayers}?`)
         this.raids[raid.quest_id].splice(raidIndex, 1)
       }
     });
 
+    /**
+     * When settings are loaded
+     * Subscribes to all previously subscribed raids
+     */
     this.settings.subscribedRaidQuestIds.forEach(raidId => {
       this.subscribeRaid(raidId);
     })
@@ -273,49 +318,6 @@ export class AppComponent implements OnInit, OnDestroy {
 
   private updateSettings() {
     localStorage.setItem('settings', JSON.stringify(this.settings));
-  }
-
-
-  // Search autocomplete
-
-  public add(event: MatChipInputEvent): void {
-    const input = event.input;
-    const value = event.value;
-    console.log(value);
-    // // Add our fruit
-    if ((value || '').trim()) {
-      // this.socketioService.subscribeRaid(raidToSub.quest_id)
-    }
-
-    // Reset the input value
-    if (input) {
-      input.value = '';
-    }
-
-    this.subbedQuestsCtrl.setValue(null);
-  }
-
-  public remove(quest: any): void {
-    this.socketioService.unsubscribeRaid(quest.quest_id);
-    this.updateSettings();
-  }
-
-  public selected(event: MatAutocompleteSelectedEvent): void {
-    this.socketioService.subscribeRaid(event.option.value.quest_id);
-    this.searchInput.nativeElement.value = '';
-    this.searchInput.nativeElement.blur();
-    this.searchInput.nativeElement.focus();
-    this.subbedQuestsCtrl.setValue(null);
-    this.matAutoComplete.openPanel();
-    this.updateSettings();
-  }
-
-  private _filter(value: string): any[] {
-    if (!value.toLowerCase) return this.raid_metadata;
-    const filterValue = value.toLowerCase();
-    const filter = this.raid_metadata.filter(raid => Object.values(raid).join().toLowerCase().includes(filterValue));
-    console.log(filterValue, filter);
-    return filter;
   }
 
   private copyTextToClipboard(input: any, { target = document.body } = {}) {
